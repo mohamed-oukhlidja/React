28aa40812ef636baa911e7523c365ca3d6fb48c9
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-01-02 12:07:50 +0100

    rendering a ul list

--- First React/main.js
+++ First React/main.js
@@ -1,1 +1,8 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+ReactDOM.render(
+  <ul>
+    <li>first</li>
+    <li>react</li>
+    <li>app</li>
+  </ul>,
+  document.getElementById("root")
+);
